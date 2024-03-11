437116ed960649573a4c63731ca61b3924c7436b
eternatus-shiny <162989820+eternatus-shiny@users.noreply.github.com>
2024-03-11 12:34:43 +0100

    Create Constants.java

---
+++ src/java/com/eternatus/modtest/Constants.java
@@ -0,0 +1,9 @@
+package com.eternatus.modtest;
+
+public class Constants {
+    public static final boolean REMAPREFS = true;
+    public static final boolean DEBUG = false;
+    public static boolean debugShaders(){
+        return false;
+    }
+}
