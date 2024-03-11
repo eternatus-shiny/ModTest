9167d952787383cb3d77cf87ef2fb74fd00f9bb0
eternatus-shiny <162989820+eternatus-shiny@users.noreply.github.com>
2024-03-11 12:52:35 +0100

    Update MTBiomeRegistry.java

--- src/java/com/eternatus/modtest/world/biome/MTBiomeRegistry.java
+++ src/java/com/eternatus/modtest/world/biome/MTBiomeRegistry.java
@@ -1,11 +1,11 @@
-package com.valeriotor.beyondtheveil.world.biome;
+package com.eternatus.modtest.world.biome;
 
-import com.valeriotor.beyondtheveil.Registration;
-import com.valeriotor.beyondtheveil.lib.References;
-import com.valeriotor.beyondtheveil.util.BiomeSampler;
-import com.valeriotor.beyondtheveil.util.ExpandedBiomes;
-import com.valeriotor.beyondtheveil.world.BiomeEntryType;
-import com.valeriotor.beyondtheveil.world.SpawnBiomeData;
+import com.eternatus.modtest.Registration;
+import com.eternatus.modtest.lib.References;
+import com.eternatus.modtest.util.BiomeSampler;
+import com.eternatus.modtest.util.ExpandedBiomes;
+import com.eternatus.modtest.world.BiomeEntryType;
+import com.eternatus.modtest.world.SpawnBiomeData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
@@ -20,27 +20,27 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.List;
 
 public class BTVBiomeRegistry {
-    public static final ResourceKey<Biome> ABYSSAL_CHASM = ResourceKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(References.MODID, "abyssal"));
-    public static final List<ResourceKey<Biome>> BEYONDTHEVEIL_BIOMES = List.of(ABYSSAL_CHASM, Registration.INNSMOUTH);
-    private static final Vec3 ABYSSAL_CHASM_LIGHT_COLOR = new Vec3(0.5, 0.5, 1);
+    public static final ResourceKey<Biome> ABYSSAL = ResourceKey.create(Registry.BIOME_REGISTRY, new ResourceLocation(References.MODID, "abyssal"));
+    public static final List<ResourceKey<Biome>> Modtest_BIOMES = List.of(ABYSSAL, Registration.INNSMOUTH);
+    private static final Vec3 ABYSSAL_LIGHT_COLOR = new Vec3(0.5, 0.5, 1);
     private static final Vec3 DEFAULT_LIGHT_COLOR = new Vec3(1, 1, 1);
 
     public static void init(){
-        ExpandedBiomes.addExpandedBiome(ABYSSAL_CHASM, LevelStem.OVERWORLD);
+        ExpandedBiomes.addExpandedBiome(ABYSSAL, LevelStem.OVERWORLD);
     }
 
     public static float getBiomeAmbientLight(Holder<Biome> value) {
         return 0.0F;
     }
     public static float getBiomeFogNearness(Holder<Biome> value) {
-        if (value.is(ABYSSAL_CHASM)) {
+        if (value.is(ABYSSAL)) {
             return -0.3F;
         }
         return 1.0F;
     }
 
     public static float getBiomeWaterFogFarness(Holder<Biome> value) {
-        if (value.is(ABYSSAL_CHASM)) {
+        if (value.is(ABYSSAL)) {
             return 0.5F;
         }
         return 1.0F;
@@ -48,7 +48,7 @@ public class BTVBiomeRegistry {
 
     public static float getBiomeSkyOverride(Holder<Biome> value) {
 
-        if (value.is(ABYSSAL_CHASM)) {
+        if (value.is(ABYSSAL)) {
             return 1.0F;
         }
         return 0.0F;
@@ -56,7 +56,7 @@ public class BTVBiomeRegistry {
 
     public static Vec3 getBiomeLightColorOverride(Holder<Biome> value) {
 
-        if (value.is(ABYSSAL_CHASM)) {
+        if (value.is(ABYSSAL)) {
             return ABYSSAL_CHASM_LIGHT_COLOR;
         }
         return DEFAULT_LIGHT_COLOR;
@@ -64,7 +64,7 @@ public class BTVBiomeRegistry {
     public static float calculateBiomeSkyOverride(Entity player) {
         int i = Minecraft.getInstance().options.biomeBlendRadius().get();
         if (i == 0) {
-            return BTVBiomeRegistry.getBiomeSkyOverride(player.getLevel().getBiome(player.blockPosition()));
+            return MTBiomeRegistry.getBiomeSkyOverride(player.getLevel().getBiome(player.blockPosition()));
         } else {
             return BiomeSampler.sampleBiomesFloat(player.getLevel(), player.position(), BTVBiomeRegistry::getBiomeSkyOverride);
         }
